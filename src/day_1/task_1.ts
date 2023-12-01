3519db8fab8ac14c199efecd62ba7d08bd36e82e
Edward Burroughes <edward@land.tech>
2023-12-01 19:37:15 +0000

    adding two pointers version

--- src/day_1/task_1.ts
+++ src/day_1/task_1.ts
@@ -1,15 +1,15 @@
 import fs from "fs";
 
-const number_map: any = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
+const number_map: { [key: string]: string } = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
 };
 
 
@@ -17,7 +17,6 @@ const extractNumbersRe = (inputString: string): string[] => {
   const re = /(?=(one|two|three|four|five|six|seven|eight|nine|\d))/g
   return Array.from(inputString.matchAll(re), x => x[1])
 };
-
 
 const convertNumber = (num_str: string): string => {
   const result = number_map[num_str] || undefined;
@@ -59,3 +58,6 @@ const getValue = (filePath: string):number => {
 }
 
 console.log(getValue("/Users/edward/code/advent_of_code/src/day_1/input_1.txt"));
+
+
+
